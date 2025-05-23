11905b85f79f07909e164e4278e8817b3609e864
danny45694 <danieladiazop@gmail.com>
2025-05-22 21:19:34 -0700

    Swapping again

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 
 
-fetch("https://api.sampleapis.com/coffee/iced")
-    .then(response => response.json())
+
+
+async function fetchData() {
+    const requestURL = "https://api.sampleapis.com/coffee/iced"
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const coffees = await response.json();
+
+    console.log(coffees)
+
+   /*  .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
+
+    const data = response.json();
+    */
+
+    const title = coffees[0].title;
+    console.log(title)
+}
+
