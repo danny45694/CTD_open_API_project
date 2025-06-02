8a5e7968020d5db209fa12f95acaa41acc266069
Daniel Desktop <danieladiazop@gmail.com>
2025-06-01 23:25:56 -0700

    Got hot/cold buttons working, fixed design issues, broke API, dabbled with try catch, changed around the javascript code

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 
 let cards = document.querySelectorAll(".card")
 
-const requestURL = "https://api.sampleapis.com/coffee/"
-const btnIced = document.getElementById("btnIced")
+const requestURL = "https://api.sampleapis.com/coffee/";
+const btnIced = document.getElementById("btnIced");
 const btnHot = document.getElementById("btnHot");
 
 const cardsContainer = document.getElementById("cardsContainer");
 
+function clearUI() {
+    cardsContainer.innerHTML = "";
+}
+
 function createCoffeeCard(object) {
-    let card = document.createElement("div");
+    const card = document.createElement("div");
     card.classList.add("card"); // Add class to card
 
-    let img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = object.image;
 
     const title = document.createElement("p");
@@ -24,72 +28,59 @@ function createCoffeeCard(object) {
     return card;
 }
 
-async function fetchData() {
-    const requestURL = "https://api.sampleapis.com/coffee/iced"
-    const request = new Request(requestURL);
-    const response = await fetch(request);
+async function fetchIcedCoffee() {
+  clearUI();
+  btnIced.classList.add("active");
+  btnHot.classList.remove("active");
+
+  try {
+    const response = await fetch(requestURL + "iced");
+    if (!response.ok) {
+      throw new Error(`Server returned ${response.status}`);
+    }
+
     const coffees = await response.json();
 
-    console.log(coffees)
-
-   /*  .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-
-    const data = response.json();
-    */
-
-    const card1 = document.getElementById("card1");
-    const card2 = document.getElementById("card2");
-    const card3 = document.getElementById("card3");
-    const card4 = document.getElementById("card4");
-    const card5 = document.getElementById("card5");
-    const card6 = document.getElementById("card6");
-    
-    const coffee1 = coffees[0].image;
-    const coffee2 = coffees[1].image;
-    const coffee3 = coffees[2].image;
-    const coffee4 = coffees[3].image;
-    const coffee5 = coffees[4].image;
-    const coffee6 = coffees[5].image;
-
-
-    card1.src = coffee1;
-    card2.src = coffee2;
-    card3.src = coffee3;
-    card4.src = coffee4;
-    card5.src = coffee5;
-    card6.src = coffee6;
-
-    card1.style.display = "block"
-    card2.style.display = "block"
-    card3.style.display = "block"
-    card4.style.display = "block"
-    card5.style.display = "block"
-    card6.style.display = "block"
-
-
-    //Append object image element from API to
-    // created image element
-
-    //img.innerHTML = coffees[0].image
-
-    //add image element to card
-
-    card1.appendChild(img);
-
-    const title = coffees[0].title;
-    console.log(title)
+    coffees.forEach(coffeeItem => {
+      const oneCard = createCoffeeCard(coffeeItem);
+      cardsContainer.appendChild(oneCard);
+    });
+  } catch (err) {
+    showMessage(`Error fetching iced coffee: ${err.message}`, true);
+  }
 }
 
-console.log(typeof cards);
-console.log(cards)
-console.log(cards.length);
 
-fetchData();
+async function fetchHotCoffee() {
+  clearUI();
+  btnHot.classList.add("active");
+  btnIced.classList.remove("active");
 
+  try {
+    const response = await fetch(requestURL + "hot");
+    if (!response.ok) {
+      throw new Error(`Server returned ${response.status}`);
+    }
 
+    const coffees = await response.json();
 
+    coffees.forEach(coffeeItem => {
+      const oneCard = createCoffeeCard(coffeeItem);
+      cardsContainer.appendChild(oneCard);
+    });
+  } catch (err) {
+    showMessage(`Error fetching hot coffee: ${err.message}`, true);
+  }
+}
 
-//let card1 = cards.querySelector("#card1");
-//console.log(card1);
+window.addEventListener("DOMContentLoaded", () => {
+    fetchIcedCoffee();
+
+    btnIced.addEventListener("click", () => {
+        fetchIcedCoffee();
+    });
+
+    btnHot.addEventListener("click", () => {
+        fetchHotCoffee();
+    });
+});
